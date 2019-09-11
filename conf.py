a305ece29093eb6f28111048875d573c8b3b6c28
Thorvaldur Konradsson <tobbetto@users.noreply.github.com>
2019-09-11 17:13:52 +0200

    Add files via upload

--- conf.py
+++ conf.py
@@ -23,6 +23,7 @@ author = 'Vector 2019'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
+master_doc = 'index'
 
 
 # -- General configuration ---------------------------------------------------
